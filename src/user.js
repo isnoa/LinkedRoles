4ef95a801d5a80320daa66ee31a791669e968e90
NOA <isnoa3166@gmail.com>
2023-03-12 12:02:29 +0900

    v1

---
+++ src/user.js
@@ -0,0 +1,24 @@
+// import mongoose from "mongoose";
+
+// mongoose.connect(process.env.MONGOOSE_URI, { useNewUrlParser: true })
+// .then(() => console.log('MongoDB Conneted'))
+// .catch(err => console.log(err));
+
+// function database() {
+//     const Schema = new mongoose.Schema({
+//         since : String,
+//         user : String,
+//         nowcharacter : String,
+//         profileconnect : Boolean,
+//         description : String,
+//         zzzconnect : String,
+//         uid : Number,
+//         dailycheckin : Boolean,
+//         ZZZCreatedAt : String,
+//         ZZZLevel : Number
+//     }, { versionKey: false })
+    
+//     mongoose.model('user', Schema)
+// };
+
+// export database;
